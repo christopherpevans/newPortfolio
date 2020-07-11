dded5238d41c09631201edb0af84c533b6ae6e1f
princetonevans <princetonevans@yahoo.com>
2020-07-11 17:49:06 -0500

    added style sheets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, FeaturesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     FontAwesomeModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
