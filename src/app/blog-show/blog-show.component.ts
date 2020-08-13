2d1d2152e84bee6a51b241d098b21e2b19e7f8a4
princetonevans <princetonevans@yahoo.com>
2020-08-13 13:13:48 -0500

    created blog show component

---
+++ src/app/blog-show/blog-show.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-show',
+  templateUrl: './blog-show.component.html',
+  styleUrls: ['./blog-show.component.scss'],
+})
+export class BlogShowComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
